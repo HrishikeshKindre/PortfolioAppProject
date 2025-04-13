02d55e47069b01c512d1aed81aeb52dbd7009a16
Anonnee <hrishikindrej@gmail.com>
2025-04-13 19:42:55 -0400

    Set up BrowserRouter and base routing structure

---
+++ src/App.js
@@ -0,0 +1,27 @@
+// Created by Hrishikesh Kindre for PortfolioAppProject
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <main className="container py-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+    </Router>
+  );
+}
+
+export default App;
